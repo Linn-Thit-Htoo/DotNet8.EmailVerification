9347c9ceab01f5f8d03b7ab97278e1c9a4b39310
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-21 21:35:34 +0630

    [ Added ] HttpClientService

---
+++ DotNet8.EmailVerification.App/Pages/Auth/P_Register.razor.cs
@@ -0,0 +1,14 @@
+﻿using DotNet8.EmailVerification.DTOs.Features.Account;
+
+namespace DotNet8.EmailVerification.App.Pages.Auth
+{
+    public partial class P_Register
+    {
+        public RegisterUserDto RequestModel { get; set; }
+
+        public async Task Register()
+        {
+
+        }
+    }
+}
---
+++ DotNet8.EmailVerification.App/Services/HttpClientService.cs
@@ -0,0 +1,63 @@
+﻿using DotNet8.EmailVerification.Shared;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DotNet8.EmailVerification.App.Services
+{
+    public class HttpClientService
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpClientService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<T> ExecuteAsync<T>(string endpoint, HttpMethod httpMethod, object? requestModel = null)
+        {
+            HttpResponseMessage? response = null;
+            HttpContent? content = null;
+
+            if (requestModel is not null)
+            {
+                string jsonStr = requestModel.ToJson();
+                content = new StringContent(jsonStr, Encoding.UTF8, Application.Json);
+            }
+
+            switch (httpMethod)
+            {
+                case HttpMethod.GET:
+                    response = await _httpClient.GetAsync(endpoint);
+                    break;
+                case HttpMethod.POST:
+                    response = await _httpClient.PostAsync(endpoint, content);
+                    break;
+                case HttpMethod.PUT:
+                    response = await _httpClient.PutAsync(endpoint, content);
+                    break;
+                case HttpMethod.PATCH:
+                    response = await _httpClient.PatchAsync(endpoint, content);
+                    break;
+                case HttpMethod.DELETE:
+                    response = await _httpClient.DeleteAsync(endpoint);
+                    break;
+                case HttpMethod.None:
+                default:
+                    throw new Exception("Invalid Http Method.");
+            }
+
+            var responseJson = await response.Content.ReadAsStringAsync();
+            return responseJson.ToObject<T>();
+        }
+    }
+
+    public enum HttpMethod
+    {
+        None,
+        GET,
+        POST,
+        PUT,
+        PATCH,
+        DELETE
+    }
+}
