1210ee9a346b52ab9a06116d39a7c16f8125cc8b
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-22 15:01:54 +0630

    Format Code

--- DotNet8.EmailVerification.Modules.Account.Infrastructure/Db/AccountDbContext.cs
+++ DotNet8.EmailVerification.Modules.Account.Infrastructure/Db/AccountDbContext.cs
@@ -1,19 +1,12 @@
 ﻿using DotNet8.EmailVerification.Modules.Account.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet8.EmailVerification.Modules.Account.Infrastructure.Db
+namespace DotNet8.EmailVerification.Modules.Account.Infrastructure.Db;
+
+public class AccountDbContext : DbContext
 {
-    public class AccountDbContext : DbContext
-    {
-        public AccountDbContext(DbContextOptions options) : base(options)
-        {
-        }
+    public AccountDbContext(DbContextOptions options)
+        : base(options) { }
 
-        public DbSet<Tbl_User> Tbl_Users { get; set; }
-        public DbSet<Tbl_Setup> Tbl_Setups { get; set; }
-    }
+    public DbSet<Tbl_User> Tbl_Users { get; set; }
+    public DbSet<Tbl_Setup> Tbl_Setups { get; set; }
 }
