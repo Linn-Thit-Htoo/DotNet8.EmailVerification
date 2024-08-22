5880cd3c3c8329fa6f98f8098611eec28a870f95
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-22 15:00:58 +0630

    Format Code

--- DotNet8.EmailVerification.Modules.Account.Domain/Entities/Tbl_Setup.cs
+++ DotNet8.EmailVerification.Modules.Account.Domain/Entities/Tbl_Setup.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet8.EmailVerification.Modules.Account.Domain.Entities
+namespace DotNet8.EmailVerification.Modules.Account.Domain.Entities;
+
+[Table("Tbl_Setup")]
+public class Tbl_Setup
 {
-    [Table("Tbl_Setup")]
-    public class Tbl_Setup
-    {
-        [Key]
-        public string SetupId { get; set; }
-        public string UserId { get; set; }
-        public string SetupCode { get; set; }
-        public DateTime CreatedDate { get; set; }
-    }
+    [Key]
+    public string SetupId { get; set; }
+    public string UserId { get; set; }
+    public string SetupCode { get; set; }
+    public DateTime CreatedDate { get; set; }
 }
