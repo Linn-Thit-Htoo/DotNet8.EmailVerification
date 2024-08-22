7f9f8c17df148bb4695932b614c48f0b8097c03b
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-22 14:58:06 +0630

    Format Code

--- DotNet8.EmailVerification.Utils/Enums/EnumStatusCode.cs
+++ DotNet8.EmailVerification.Utils/Enums/EnumStatusCode.cs
@@ -1,22 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.EmailVerification.Utils.Enums;
 
-namespace DotNet8.EmailVerification.Utils.Enums
+public enum EnumStatusCode
 {
-    public enum EnumStatusCode
-    {
-        Success = 200,
-        Created = 201,
-        Accepted = 202,
-        BadRequest = 400,
-        UnAuthorized = 401,
-        NotFound = 404,
-        MethodNotAllowed = 405,
-        Conflict = 409,
-        Locked = 423,
-        InternalServerError = 500
-    }
+    Success = 200,
+    Created = 201,
+    Accepted = 202,
+    BadRequest = 400,
+    UnAuthorized = 401,
+    NotFound = 404,
+    MethodNotAllowed = 405,
+    Conflict = 409,
+    Locked = 423,
+    InternalServerError = 500
 }
