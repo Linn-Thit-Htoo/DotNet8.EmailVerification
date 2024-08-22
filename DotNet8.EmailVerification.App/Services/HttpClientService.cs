592ae8e4f2d980ba7080a1e7a447ebdc5d465ac7
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-22 14:59:07 +0630

    Format Code

--- DotNet8.EmailVerification.App/Services/HttpClientService.cs
+++ DotNet8.EmailVerification.App/Services/HttpClientService.cs
@@ -1,63 +1,58 @@
-﻿using DotNet8.EmailVerification.Shared;
-using System.Text;
-using static System.Net.Mime.MediaTypeNames;
+﻿namespace DotNet8.EmailVerification.App.Services;
 
-namespace DotNet8.EmailVerification.App.Services
+public class HttpClientService
 {
-    public class HttpClientService
+    private readonly HttpClient _httpClient;
+
+    public HttpClientService(HttpClient httpClient)
     {
-        private readonly HttpClient _httpClient;
-
-        public HttpClientService(HttpClient httpClient)
-        {
-            _httpClient = httpClient;
-        }
-
-        public async Task<T> ExecuteAsync<T>(string endpoint, EnumHttpMethod httpMethod, object? requestModel = null)
-        {
-            HttpResponseMessage? response = null;
-            HttpContent? content = null;
-
-            if (requestModel is not null)
-            {
-                string jsonStr = requestModel.ToJson();
-                content = new StringContent(jsonStr, Encoding.UTF8, Application.Json);
-            }
-
-            switch (httpMethod)
-            {
-                case EnumHttpMethod.GET:
-                    response = await _httpClient.GetAsync(endpoint);
-                    break;
-                case EnumHttpMethod.POST:
-                    response = await _httpClient.PostAsync(endpoint, content);
-                    break;
-                case EnumHttpMethod.PUT:
-                    response = await _httpClient.PutAsync(endpoint, content);
-                    break;
-                case EnumHttpMethod.PATCH:
-                    response = await _httpClient.PatchAsync(endpoint, content);
-                    break;
-                case EnumHttpMethod.DELETE:
-                    response = await _httpClient.DeleteAsync(endpoint);
-                    break;
-                case EnumHttpMethod.None:
-                default:
-                    throw new Exception("Invalid Http Method.");
-            }
-
-            var responseJson = await response.Content.ReadAsStringAsync();
-            return responseJson.ToObject<T>();
-        }
+        _httpClient = httpClient;
     }
 
-    public enum EnumHttpMethod
+    public async Task<T> ExecuteAsync<T>(string endpoint, EnumHttpMethod httpMethod, object? requestModel = null)
     {
-        None,
-        GET,
-        POST,
-        PUT,
-        PATCH,
-        DELETE
+        HttpResponseMessage? response = null;
+        HttpContent? content = null;
+
+        if (requestModel is not null)
+        {
+            string jsonStr = requestModel.ToJson();
+            content = new StringContent(jsonStr, Encoding.UTF8, Application.Json);
+        }
+
+        switch (httpMethod)
+        {
+            case EnumHttpMethod.GET:
+                response = await _httpClient.GetAsync(endpoint);
+                break;
+            case EnumHttpMethod.POST:
+                response = await _httpClient.PostAsync(endpoint, content);
+                break;
+            case EnumHttpMethod.PUT:
+                response = await _httpClient.PutAsync(endpoint, content);
+                break;
+            case EnumHttpMethod.PATCH:
+                response = await _httpClient.PatchAsync(endpoint, content);
+                break;
+            case EnumHttpMethod.DELETE:
+                response = await _httpClient.DeleteAsync(endpoint);
+                break;
+            case EnumHttpMethod.None:
+            default:
+                throw new Exception("Invalid Http Method.");
+        }
+
+        var responseJson = await response.Content.ReadAsStringAsync();
+        return responseJson.ToObject<T>();
     }
+}
+
+public enum EnumHttpMethod
+{
+    None,
+    GET,
+    POST,
+    PUT,
+    PATCH,
+    DELETE
 }
