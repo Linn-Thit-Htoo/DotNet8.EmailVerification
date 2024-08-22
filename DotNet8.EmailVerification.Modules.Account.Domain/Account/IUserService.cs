2eae9a121bc37348351caf91a5ece994eaf9afe6
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-22 15:00:39 +0630

    Format Code

--- DotNet8.EmailVerification.Modules.Account.Domain/Account/IUserService.cs
+++ DotNet8.EmailVerification.Modules.Account.Domain/Account/IUserService.cs
@@ -1,16 +1,7 @@
-﻿using DotNet8.EmailVerification.DTOs.Features.Account;
-using DotNet8.EmailVerification.Utils;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.EmailVerification.Modules.Account.Domain.Account;
 
-namespace DotNet8.EmailVerification.Modules.Account.Domain.Account
+public interface IUserService
 {
-    public interface IUserService
-    {
-        Task<Result<UserDto>> RegisterAsync(RegisterUserDto registerUser, CancellationToken cancellationToken);
-        Task<Result<UserDto>> ConfirmEmailAsync(ConfirmEmailRequestDto confirmEmailRequest, CancellationToken cancellationToken);
-    }
+    Task<Result<UserDto>> RegisterAsync(RegisterUserDto registerUser, CancellationToken cancellationToken);
+    Task<Result<UserDto>> ConfirmEmailAsync(ConfirmEmailRequestDto confirmEmailRequest, CancellationToken cancellationToken);
 }
