f58f165e27d41c3adf9bbc4613f7b1b929dd6ebe
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-22 15:00:00 +0630

    Format Code

--- DotNet8.EmailVerification.App/Pages/Auth/P_Register.razor.cs
+++ DotNet8.EmailVerification.App/Pages/Auth/P_Register.razor.cs
@@ -1,22 +1,25 @@
-﻿namespace DotNet8.EmailVerification.App.Pages.Auth
-{
-    public partial class P_Register
-    {
-        public RegisterUserDto RequestModel { get; set; } = new();
+﻿namespace DotNet8.EmailVerification.App.Pages.Auth;
 
-        public async Task Register()
+public partial class P_Register
+{
+    public RegisterUserDto RequestModel { get; set; } = new();
+
+    public async Task Register()
+    {
+        var result = await HttpClientService.ExecuteAsync<Result<UserDto>>(
+            "/api/account/register",
+            EnumHttpMethod.POST,
+            RequestModel
+        );
+        if (result.IsSuccess)
         {
-            var result = await HttpClientService.ExecuteAsync<Result<UserDto>>("/api/account/register", EnumHttpMethod.POST, RequestModel);
-            if (result.IsSuccess)
-            {
-                await LocalStorage.SetItemAsStringAsync("UserId", result.Data.UserId);
-                Snackbar.Add(result.Message, Severity.Success);
-                Navigation.NavigateTo("/confirm-email");
-            }
-            else
-            {
-                Snackbar.Add(result.Message, Severity.Error);
-            }
+            await LocalStorage.SetItemAsStringAsync("UserId", result.Data.UserId);
+            Snackbar.Add(result.Message, Severity.Success);
+            Navigation.NavigateTo("/confirm-email");
+        }
+        else
+        {
+            Snackbar.Add(result.Message, Severity.Error);
         }
     }
 }
