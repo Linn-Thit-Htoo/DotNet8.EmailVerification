d90ca2535e7d4fad7c10895308f2733815d358d1
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-22 15:01:05 +0630

    Format Code

--- DotNet8.EmailVerification.Modules.Account.Domain/Entities/Tbl_Setup.cs
+++ DotNet8.EmailVerification.Modules.Account.Domain/Entities/Tbl_Setup.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace DotNet8.EmailVerification.Modules.Account.Domain.Entities;
+﻿namespace DotNet8.EmailVerification.Modules.Account.Domain.Entities;
 
 [Table("Tbl_Setup")]
 public class Tbl_Setup
--- DotNet8.EmailVerification.Modules.Account.Domain/GlobalUsings.cs
+++ DotNet8.EmailVerification.Modules.Account.Domain/GlobalUsings.cs
@@ -1,4 +1,6 @@
 // Global using directives
 
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
 global using DotNet8.EmailVerification.DTOs.Features.Account;
 global using DotNet8.EmailVerification.Utils;
