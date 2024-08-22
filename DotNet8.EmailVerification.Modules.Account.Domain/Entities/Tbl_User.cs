1a48763676fbf8e0e9f0af6333a65c3993947549
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-22 15:01:15 +0630

    Format Code

--- DotNet8.EmailVerification.Modules.Account.Domain/Entities/Tbl_User.cs
+++ DotNet8.EmailVerification.Modules.Account.Domain/Entities/Tbl_User.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.EmailVerification.Modules.Account.Domain.Entities;
 
-namespace DotNet8.EmailVerification.Modules.Account.Domain.Entities
+[Table("Tbl_User")]
+public class Tbl_User
 {
-    [Table("Tbl_User")]
-    public class Tbl_User
-    {
-        [Key]
-        public string UserId { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsActive { get; set; }
-        public bool IsEmailVerified { get; set; }
-    }
+    [Key]
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsEmailVerified { get; set; }
 }
