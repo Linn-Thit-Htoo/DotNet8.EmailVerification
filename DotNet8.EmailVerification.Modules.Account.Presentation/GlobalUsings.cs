4c2f2c17e22fa6c882160fc05841829f434df858
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-22 15:02:45 +0630

    Format Code

---
+++ DotNet8.EmailVerification.Modules.Account.Presentation/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global using directives
+
+global using DotNet8.EmailVerification.DTOs.Features.Account;
+global using DotNet8.EmailVerification.Modules.Account.Domain.Account;
+global using Microsoft.AspNetCore.Mvc;
