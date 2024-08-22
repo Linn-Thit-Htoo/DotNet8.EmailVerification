f7d3ade7f038b27d546a76414fd05ce0d97150a8
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-22 14:57:04 +0630

    Format Code

--- DotNet8.EmailVerification.DTOs/Features/Account/UserDTO.cs
+++ DotNet8.EmailVerification.DTOs/Features/Account/UserDTO.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.EmailVerification.DTOs.Features.Account;
 
-namespace DotNet8.EmailVerification.DTOs.Features.Account
+public class UserDto
 {
-    public class UserDto
-    {
-        public string UserId { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsActive { get; set; }
-        public bool IsEmailVerified { get; set; }
-    }
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsEmailVerified { get; set; }
 }
