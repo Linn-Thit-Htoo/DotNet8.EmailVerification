6941e33fdc7ca803f6641bdd03a639386d59d8b8
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-22 14:57:39 +0630

    Format Code

--- DotNet8.EmailVerification.Shared/DevCode.cs
+++ DotNet8.EmailVerification.Shared/DevCode.cs
@@ -1,16 +1,10 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet8.EmailVerification.Shared
+namespace DotNet8.EmailVerification.Shared;
+
+public static class DevCode
 {
-    public static class DevCode
-    {
-        public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj);
+    public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj);
 
-        public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
-    }
+    public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
 }
