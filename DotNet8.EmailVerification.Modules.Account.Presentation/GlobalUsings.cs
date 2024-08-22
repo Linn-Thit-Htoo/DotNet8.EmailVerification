d3c91a0f78b41dc2ae25c7606727213420cf82d9
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-22 15:03:41 +0630

    Format Code

--- DotNet8.EmailVerification.Modules.Account.Presentation/GlobalUsings.cs
+++ DotNet8.EmailVerification.Modules.Account.Presentation/GlobalUsings.cs
@@ -4,6 +4,7 @@ global using DotNet8.EmailVerification.DTOs.Features.Account;
 global using DotNet8.EmailVerification.Modules.Account.Domain.Account;
 global using DotNet8.EmailVerification.Modules.Account.Infrastructure.Account;
 global using DotNet8.EmailVerification.Modules.Account.Infrastructure.Db;
+global using DotNet8.EmailVerification.Modules.Account.Presentation.Extensions;
 global using DotNet8.EmailVerification.Shared;
 global using Hangfire;
 global using Microsoft.AspNetCore.Mvc;
--- DotNet8.EmailVerification.Modules.Account.Presentation/Program.cs
+++ DotNet8.EmailVerification.Modules.Account.Presentation/Program.cs
@@ -1,10 +1,9 @@
-using DotNet8.EmailVerification.Modules.Account.Presentation.Extensions;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
