5c2356233d171a605715aaa7ea1368fff0f92c77
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-22 15:01:41 +0630

    Format Code

---
+++ DotNet8.EmailVerification.Modules.Account.Infrastructure/GlobalUsings.cs
@@ -0,0 +1,11 @@
+// Global using directives
+
+global using DotNet8.EmailVerification.DTOs.Features.Account;
+global using DotNet8.EmailVerification.DTOs.Features.Setup;
+global using DotNet8.EmailVerification.Modules.Account.Domain.Account;
+global using DotNet8.EmailVerification.Modules.Account.Infrastructure.Db;
+global using DotNet8.EmailVerification.Modules.Account.Infrastructure.Extensions;
+global using DotNet8.EmailVerification.Utils;
+global using FluentEmail.Core;
+global using Hangfire;
+global using Microsoft.EntityFrameworkCore;
